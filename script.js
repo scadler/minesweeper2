416e367a8daa1505c1022b48ce38b70715e41680
scadler <scadler@s.sfusd.edu>
2021-03-25 04:46:32 +0000

    deleted unneeded code

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+$("#imagesDiv").hide();
 var ctx = canvas.getContext('2d');
 var tile = [];
 /*
     [ [0] [1] [1] ]
-    [ [1] [3] [M] ]
-    [ [1] [M] [M] ]
+    [ [1] [3] [X] ]
+    [ [1] [X] [X] ]
     tile[0][2] = 1, tile[1][1] = 3
+     _ = null, X = mine, nums 0-8 = 0-8, f = flagged mine, n = flagged number
 */ 
-/*
-    _ = null, M = mine, nums 0-8 = 0-8, f = flagged mine, n = flagged number
-*/
+var imageList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight"]  
 var neighborIndexMath = [[-1,-1], [0,-1], [1,-1], [-1,0], [1,0], [-1,1], [0,1], [1,1]];
 var rows = 10;
 var cols = 10;
@@ -46,8 +46,8 @@ function generateBombs(){
     while(i<20){
         var x = Math.floor(Math.random()*10);
         var y = Math.floor(Math.random()*10);
-        if(tile[x][y]!= "M"){
-        tile[x][y] = "M";
+        if(tile[x][y]!= "X"){
+        tile[x][y] = "X";
         i++;
         }
     }
@@ -56,15 +56,15 @@ function generateBombs(){
 function generateTiles(){
       for ( var i = 0; i<rows; i++){
         for ( var j = 0; j<cols; j++){
-            if(tile[i][j] !== "M"){
+            if(tile[i][j] !== "X"){
                 var ii = 0;
                 var neighbors = 0;
                 while(ii<8){
                     x = neighborIndexMath[ii][0];
                     y = neighborIndexMath[ii][1];
-                    if((i+x)>= 0 && (j+y)>= 0 && (i+x) <= 9 && (j+y)<= 9){
+                    if((i+x)>=0 && (j+y)>=0 && (i+x)<=9 && (j+y)<=9){
                         //this if statement is to make sure that tile[i+x][j+y] exists
-                        neighbors += (tile[i+x][j+y] === "M") ? 1 : 0;
+                        neighbors += (tile[i+x][j+y] === "X") ? 1 : 0;
                     }
                     ii++;
                 }
@@ -75,3 +75,16 @@ function generateTiles(){
     console.log(tile)
 }
 generateArray();
+
+function tileClicked(row,col){
+    var i = Math.floor(col/50)
+    var j = Math.floor(row/50)
+    if(i>=0 && j>=0 && i<=9 && j<=9){
+        let image = (imageList[tile[i][j]]) ? document.getElementById(`${imageList[tile[i][j]]}`): document.getElementById("mine");
+        console.log(tile[i][j]+" "+col+" "+row+" "+i+" "+j+" "+imageList[tile[i][j]])
+            ctx.drawImage(image, j*50, i*50, 50, 50);
+        }
+    }
+document.getElementById("canvas").addEventListener('click', (event) => {
+    tileClicked(event.pageX-15,event.pageY-15)
+});
